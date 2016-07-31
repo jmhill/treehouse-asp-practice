0101e3d920c1e656297c62681d371aa415f9dce5
Justin Hill <justinmhill@gmail.com>
2016-07-31 15:09:31 -0400

    Change return type to ActionResult

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,9 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from comic book controller";
+            return Content("Hello from comic book controller");
         }
     }
 }
